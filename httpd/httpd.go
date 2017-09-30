35d1be3172979fb082fb14692d67ff15229bb360
as <as.utf8@gmail.com>
2017-09-29 18:45:04 -0700

    gofmt: httpd

--- httpd/httpd.go
+++ httpd/httpd.go
@@ -1,23 +1,22 @@
 package main
 
-import(
+import (
+	"log"
 	"net/http"
 	"os"
-	"log"
 )
 
-func main(){
+func main() {
 	a := os.Args
 	dir := "."
 	port := ":8080"
-	if len(a) > 1{
+	if len(a) > 1 {
 		dir = a[1]
 	}
-	if len(a) > 2{
+	if len(a) > 2 {
 		port = a[2]
 	}
-	if err := http.ListenAndServe(port, http.FileServer(http.Dir(dir))); err != nil{
-		log.Fatalln("httpd:",err)
+	if err := http.ListenAndServe(port, http.FileServer(http.Dir(dir))); err != nil {
+		log.Fatalln("httpd:", err)
 	}
 }
-		
