94787a0b09cc79da3d0bc3de81db2c2b1ce809b5
as <as.utf8@gmail.com>
2017-09-29 18:45:17 -0700

    gofmt: pv

--- pv/pv.go
+++ pv/pv.go
@@ -1,36 +1,40 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
 	"os"
 	"time"
-	"log"
-	"io"
-	"fmt"
-	"bufio"
 )
 
 type Unit int64
 
-func (u Unit) String() string{
-	const(
-		G = 1<<30
-		M = 1<<20
-		K = 1<<10
-		B = 1<<1
+func (u Unit) String() string {
+	const (
+		G  = 1 << 30
+		M  = 1 << 20
+		K  = 1 << 10
+		B  = 1 << 1
 		fm = "%.3f %s"
 	)
 	switch {
-	case u >= G: return fmt.Sprintf(fm, float64(u)/G, "G")
-	case u >= M: return fmt.Sprintf(fm, float64(u)/M, "M")
-	case u >= K: return fmt.Sprintf(fm, float64(u)/K, "K")
-	default:     return fmt.Sprintf(fm, float64(u)/B, "B")
+	case u >= G:
+		return fmt.Sprintf(fm, float64(u)/G, "G")
+	case u >= M:
+		return fmt.Sprintf(fm, float64(u)/M, "M")
+	case u >= K:
+		return fmt.Sprintf(fm, float64(u)/K, "K")
+	default:
+		return fmt.Sprintf(fm, float64(u)/B, "B")
 	}
 }
 
-func init(){
+func init() {
 	log.SetFlags(0)
 	if len(os.Args) > 1 {
-		log.SetPrefix(os.Args[1]+": ")
+		log.SetPrefix(os.Args[1] + ": ")
 	} else {
 		log.SetPrefix("pv: ")
 	}
@@ -38,7 +42,7 @@ func init(){
 
 func main() {
 	var (
-		back [1024*1024]byte
+		back       [1024 * 1024]byte
 		sec, total int64
 	)
 	done := make(chan bool)
@@ -46,14 +50,14 @@ func main() {
 	pipefd := io.TeeReader(bufio.NewReader(os.Stdin), os.Stdout)
 	tick := time.NewTicker(time.Second * 1)
 
-	go func(){
+	go func() {
 		// Copy data from the pipe to back buffer and send the
 		// write size through nchan.
-		for{
+		for {
 			n, err := pipefd.Read(back[:])
 			nchan <- int64(n)
 			if err != nil {
-				if err != io.EOF{
+				if err != io.EOF {
 					log.Println(err)
 				}
 				done <- true
@@ -69,10 +73,10 @@ func main() {
 			ts := start.Fmt(t)
 			log.Printf("t=%d   b=%s   Δ=%s/s  a=%s/s\n", ts, Unit(total), Unit(sec), Unit(total/int64(ts+1)))
 			sec = 0
-		case n := <- nchan:
-			sec += n	
+		case n := <-nchan:
+			sec += n
 			total += n
-		case <- done:
+		case <-done:
 			ts := start.Fmt(time.Now())
 			log.Printf("t=%d   b=%s   Δ=%s/s  a=%s/s\n", ts, Unit(total), Unit(sec), Unit(total/int64(ts+1)))
 			close(nchan)
@@ -83,6 +87,6 @@ func main() {
 
 type Start time.Time
 
-func (s Start) Fmt(t time.Time) int{
+func (s Start) Fmt(t time.Time) int {
 	return int(time.Since(time.Time(s)).Seconds())
 }
