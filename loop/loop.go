654c4f51677c3e88fff22c07ae5ecc7875ef5b66
as <as.utf8@gmail.com>
2018-03-18 20:11:47 -0700

    loop: gofmt -s -w

---
+++ loop/loop.go
@@ -0,0 +1,39 @@
+// loop is a temporary program for Windows systems that executes a looping command
+// this is because (for /l %a in () do (somecommand.exe)) | filter.exe crashes command prompt
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+var (
+	checkStatus = flag.Bool("ck", false, "check exit status and terminate loop if non-zero")
+)
+
+func init() {
+	log.SetFlags(0)
+	log.SetPrefix("loop:")
+	flag.Parse()
+}
+
+func main() {
+	a := flag.Args()
+	if len(a) < 1 {
+		log.Println("usage: loop cmd [args...]")
+	}
+	for {
+		out, err := exec.Command(a[0], a[1:]...).Output()
+		fmt.Print(out)
+		if err != nil {
+			e, _ := err.(*exec.ExitError)
+			if e != nil && !*checkStatus {
+				continue
+			}
+			log.Println(e)
+			break
+		}
+	}
+}
